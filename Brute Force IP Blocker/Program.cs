51b003f71437668c38689ad17192221c75c1b0e1
AlexisGross <tecnico0212@hotmail.com>
2022-06-28 00:42:42 -0300

    Novedades de esta versión: * Ya esta disponible el modo de escaneo continuo.

--- Brute Force IP Blocker/Program.cs
+++ Brute Force IP Blocker/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
 using WindowsFirewallHelper;
 using WindowsFirewallHelper.Addresses;
 
@@ -12,69 +13,133 @@ namespace Brute_Force_IP_Blocker
     {
         static void Main(string[] args)
         {
-            List<string> IPs = ReadSecurityEvents();
-            if (IPs.Count > 0)
-            {
-                Console.WriteLine("Se han detectado intentos de inicio de sesión fallidos provenientes de las siguientes IPs:");
-                for (int index = 0; index < IPs.Count; index++)
-                {
-                    Console.WriteLine(IPs[index]);
-                }
-                Console.WriteLine("\nTotal de IPs infractoras: " + IPs.Count);
-                try
-                {
-                    Console.WriteLine("Total de IPs bloqueadas actualmente: " + GetIPBloqued().RemoteAddresses.Length);
-                }
-                catch
-                {
+            Console.Title = "Brute Force IP Blocker";
+            Console.WriteLine("Este programa fue desarrollado por Alexis Gabriel Gross.");
 
-                }
-                Console.WriteLine("\n¿Desea ejecutar la aplicacion en modo bloqueo continuo?");
+            Console.WriteLine("\n¿Desea ejecutar la aplicacion en modo bloqueo continuo?");
+            Console.WriteLine("[S] Si [N] No");
+            List<string> IPs = ReadSecurityEvents();
+            ConsoleKey consoleKey = Console.ReadKey().Key;
+            if (consoleKey == ConsoleKey.S)
+            {
+                Console.WriteLine("\n¿Desea que se vacie el registro automaticamente?");
                 Console.WriteLine("[S] Si [N] No");
-                ConsoleKey consoleKey = Console.ReadKey().Key;
+                bool ClearRegistry = false;
+                consoleKey = Console.ReadKey().Key;
                 if (consoleKey == ConsoleKey.S)
                 {
-
+                    ClearRegistry = true;
                 }
                 else if (consoleKey == ConsoleKey.N)
                 {
-                    Console.WriteLine("\n\n¿Desea bloquear estas IPs en el Firewall de Windows Defender?");
+                    ClearRegistry = false;
+                }
+                int PeriodScan = 0;
+                do
+                {
+                    Console.WriteLine("\nElija la frecuencia de escaneo:");
+                    Console.WriteLine("[1] 1 minuto");
+                    Console.WriteLine("[2] 5 minutos");
+                    Console.WriteLine("[3] 15 minutos");
+                    Console.WriteLine("[4] 30 minutos");
+                    Console.WriteLine("[5] 60 minutos");
+                    string Option = Console.ReadLine();
+                    if (Option == "1" || Option == "2" || Option == "3" ||
+                         Option == "4" || Option == "4")
+                    {
+                        switch (Option)
+                        {
+                            case "1":
+                                PeriodScan = 60000;
+                                break;
+                            case "2":
+                                PeriodScan = 300000;
+                                break;
+                            case "3":
+                                PeriodScan = 900000;
+                                break;
+                            case "4":
+                                PeriodScan = 1800000;
+                                break;
+                            case "5":
+                                PeriodScan = 3600000;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                } while (PeriodScan == 0);
+
+                if (ClearRegistry)
+                {
+                    Console.WriteLine("El programa bloqueará automaticamente las nuevas IPs infractoras y vaciará el registro de eventos de seguridad.");
+                }
+                else
+                {
+                    Console.WriteLine("El programa bloqueará automaticamente las nuevas IPs infractoras, no se vaciará el registro de eventos.");
+                }
+
+                while (true)
+                {
+                    Console.WriteLine(DateTime.Now.ToString());
+                    IPs = ReadSecurityEvents();
+                    if (IPs.Count > 0)
+                    {
+                        PrintIPs(IPs);
+                        BlockIPs(IPs, GetIPBloqued());
+                        if (ClearRegistry)
+                        {
+                            DeleteSecurityEvents();
+                        }
+                    }
+                    Task.Delay(PeriodScan).Wait();
+                }
+            }
+            else if (consoleKey == ConsoleKey.N)
+            {
+                if (IPs.Count > 0)
+                {
+                    Console.WriteLine("\nSe han detectado intentos de inicio de sesión fallidos provenientes de las siguientes IPs:");
+                    PrintIPs(IPs);
+                }
+                else
+                {
+                    Console.WriteLine("No se han detectado inicio de sesión fallidos.");
+                }
+
+                Console.WriteLine("\n\n¿Desea bloquear estas IPs en el Firewall de Windows Defender?");
+                Console.WriteLine("[S] Si [N] No");
+                consoleKey = Console.ReadKey().Key;
+                if (consoleKey == ConsoleKey.S)
+                {
+                    BlockIPs(IPs, GetIPBloqued());
+                    // pregunto al usuario si desea vaciar el registro de Windows
+                    Console.WriteLine("\n\n¿Desea eliminar todos los registros de seguridad?");
                     Console.WriteLine("[S] Si [N] No");
                     consoleKey = Console.ReadKey().Key;
                     if (consoleKey == ConsoleKey.S)
                     {
-                        BlockIPs(IPs, GetIPBloqued());
-                        // pregunto al usuario si desea vaciar el registro de Windows
-                        Console.WriteLine("\n\n¿Desea eliminar todos los registros de seguridad?");
-                        Console.WriteLine("[S] Si [N] No");
-                        consoleKey = Console.ReadKey().Key;
-                        if (consoleKey == ConsoleKey.S)
-                        {
-                            DeleteSecurityEvents();
-                        }
-                        else if (consoleKey == ConsoleKey.N)
-                        {
-                            Console.WriteLine("\n\nNo se ha eliminado ningun registro de seguridad");
-                        }
-                        else
-                        {
-
-                        }
+                        DeleteSecurityEvents();
                     }
                     else if (consoleKey == ConsoleKey.N)
                     {
-                        Console.WriteLine("\nNo se ha bloqueado ninguna IP");
+                        Console.WriteLine("\n\nNo se ha eliminado ningun registro de seguridad");
                     }
                     else
                     {
 
                     }
                 }
+                else if (consoleKey == ConsoleKey.N)
+                {
+                    Console.WriteLine("\nNo se ha bloqueado ninguna IP");
+                }
+                else
+                {
+
+                }
             }
-            else
-            {
-                Console.WriteLine("No se han detectado inicio de sesión fallidos.");
-            }
+
             Console.WriteLine("");
             Console.WriteLine("Precione cualquier tecla para continuar...");
             Console.ReadKey();
@@ -168,6 +233,23 @@ namespace Brute_Force_IP_Blocker
         private static IFirewallRule GetIPBloqued()
         {
             return FirewallManager.Instance.Rules.FirstOrDefault(o => o.Direction == FirewallDirection.Inbound && o.Name.Equals("Bloqueador IP"));
+        }
+
+        private static void PrintIPs(List<string> IPs)
+        {
+            for (int index = 0; index < IPs.Count; index++)
+            {
+                Console.WriteLine(IPs[index]);
+            }
+            Console.WriteLine("\nTotal de IPs infractoras: " + IPs.Count);
+            try
+            {
+                Console.WriteLine("Total de IPs bloqueadas actualmente: " + GetIPBloqued().RemoteAddresses.Length);
+            }
+            catch
+            {
+
+            }
         }
     }
 }
